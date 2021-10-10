12b15c7c1b5470ebd919fb8e9c0ab1edda88786a
Setiawan Setiawan <setiawan.tumilin@gmail.com>
2021-10-10 08:05:37 +0700

    state, mutation & action

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0
   },
   mutations: {
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber
+    },
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    }
   },
   actions: {
+    decreaseCounter({commit}) {
+      console.log('dereaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('decreaseCounter', response.data)
+        // console.log('response : ', response)
+      })
+    },
+    increaseCounter({commit}) {
+      console.log('increaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('increaseCounter', response.data)
+        // console.log('response : ', response)
+      })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter">
+      {{ $store.state.counter }}
+    </div>
+    <div class="buttons">
+      <button @click="$store.dispatch('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+div {
+  margin-bottom: 1px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0px 10px;
+}
+</style>
